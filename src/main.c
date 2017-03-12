43ca6096bc07f6b2658f16416213471438e8a012
Link Chen <s170964@gmail.com>
2017-03-12 13:06:57 +0800

    The first time commit

---
+++ src/main.c
@@ -0,0 +1,111 @@
+//
+// This file is part of the GNU ARM Eclipse distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/Trace.h"
+
+#include "Timer.h"
+#include "BlinkLed.h"
+#include "stm32f4xx_hal_usart.h"
+
+// ----------------------------------------------------------------------------
+//
+// Semihosting STM32F4 led blink sample (trace via DEBUG).
+//
+// In debug configurations, demonstrate how to print a greeting message
+// on the trace device. In release configurations the message is
+// simply discarded.
+//
+// To demonstrate semihosting, display a message on the standard output
+// and another message on the standard error.
+//
+// Then demonstrates how to blink a led with 1 Hz, using a
+// continuous loop and SysTick delays.
+//
+// On DEBUG, the uptime in seconds is also displayed on the trace device.
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the DEBUG output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// ----- Timing definitions -------------------------------------------------
+
+// Keep the LED on for 2/3 of a second.
+#define BLINK_ON_TICKS  (TIMER_FREQUENCY_HZ * 2 / 4)
+#define BLINK_OFF_TICKS (TIMER_FREQUENCY_HZ - BLINK_ON_TICKS)
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+int
+main(int argc, char* argv[])
+{
+  USART_InitTypeDef UART1;
+
+  UART1.BaudRate = 9600;
+  // Show the program parameters (passed via semihosting).
+  // Output is via the semihosting output channel.
+  trace_dump_args(argc, argv);
+
+  // Send a greeting to the trace device (skipped on Release).
+  trace_puts("Hello ARM World!!");
+
+  // Send a message to the standard output.
+  puts("Standard output message....");
+
+  // Send a message to the standard error.
+  fprintf(stderr, "Standard error message.\n");
+
+  // At this stage the system clock should have already been configured
+  // at high speed.
+  trace_printf("System clock: %u Hz\n", SystemCoreClock);
+
+  timer_start();
+
+  blink_led_init();
+  
+  uint32_t seconds = 0;
+
+#define LOOP_COUNT (500)
+
+  int loops = LOOP_COUNT;
+  if (argc > 1)
+    {
+      // If defined, get the number of loops from the command line,
+      // configurable via semihosting.
+      loops = atoi (argv[1]);
+    }
+
+  // Short loop.
+  for (int i = 0; i < loops; i++)
+    {
+      blink_led_on();
+      timer_sleep(100);
+
+      blink_led_off();
+      timer_sleep(100);
+
+     // ++seconds;
+      // Count seconds on the trace device.
+      //trace_printf("Second %u\n", seconds);
+    }
+  return 0;
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
